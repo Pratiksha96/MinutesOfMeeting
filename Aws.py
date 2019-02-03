9e3868d9db451c5b2160df78cd3d83cf08cd20e9
tsamridh86 <t.samridh.86@gmail.com>
2019-02-03 18:33:27 +0530

    fixed aws object

--- Aws.py
+++ Aws.py
@@ -5,31 +5,42 @@ from summa import summarizer
 from summa.summarizer import summarize
 
 class Aws(object):
-    def __init__(self,s3,transcribe):
-        self.s3 = s3
-        self.transcribe = transcribe
 
-    def s3Obj(self):
+    def __init__(self,s3,transcribe):
         self.s3 = boto3.resource('s3',
         aws_access_key_id="AKIAIQL2TXLPRNX5BR6A",
         aws_secret_access_key= "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")
-        s3_client = boto3.client('s3')
+        self.s3_client = boto3.client('s3')
+        self.transcribe_client = boto3.client('transcribe')
+        self.bucketName = 'reinventorsbucket'
+
+
+    def getTextFromSpeech(self, speechFile):
+        s3URL = addToBucket(speechFile)
+        response = translateToText(speechFile.name, s3URL)
+        newFileName = speechFile.name + '.json'
+        self.s3.meta.client.download_file(self.bucket.name, newFileName, './'+newFileName)
+        with open(newFileName) as jsonResponse:
+            data = json.load(jsonResponse)
+        text = data["results"]["transcripts"][0]["transcript"]
+        return(text)
+
 
     def addToBucket(self,file_name):
-        self.bucket = self.s3.Bucket('reinventorsbucket')
+        self.bucket = self.s3.Bucket(self.bucketName)
         object_url = "https://s3.amazonaws.com/{0}/{1}".format(self.bucket.name,file_name)
         return object_url
 
-    def transcribeObj(self):
-        transcribe_client = boto3.client('transcribe')
-        response = transcribe_client.start_transcription_job(
-        TranscriptionJobName='audioinput',
+
+    def translateToText(self,newFileName,s3URL):
+        return transcribe_client.start_transcription_job(
+        TranscriptionJobName=newFileName,
         LanguageCode='en-US',
-        MediaFormat='mp4',
+        MediaFormat='mp3',
         Media={
-            'MediaFileUri': object_url
+            'MediaFileUri': s3URL
         },
-        OutputBucketName='reinventorsbucket',
+        OutputBucketName=self.bucketName,
         Settings={
             'ShowSpeakerLabels': True
             'MaxSpeakerLabels': 3,
@@ -38,21 +49,15 @@ class Aws(object):
         )
 
 
-s3.meta.client.download_file(bucket.name, 'audioinput.json', './audioinput.json')
 
-with open('audioinput.json') as f:
-    data = json.load(f)
 
-pprint(data)
-
-text = data["results"]["transcripts"][0]["transcript"]
-print(text)
-
-summary = summarizer.summarize(text)
-print(summary)
-
-summ = summarize(text, ratio=0.4)
-print(summ)
-
-with open("Summarized.txt", "w") as text_file:
-    text_file.write(summ)
+#
+#
+# summary = summarizer.summarize(text)
+# print(summary)
+#
+# summ = summarize(text, ratio=0.4)
+# print(summ)
+#
+# with open("Summarized.txt", "w") as text_file:
+#     text_file.write(summ)
